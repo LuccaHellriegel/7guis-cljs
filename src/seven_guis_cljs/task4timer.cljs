53fb6c5f97bfbce6ebc0461f0c6049c000329bbd
LuccaHellriegel <lucca.hellriegel@gmx.net>
2021-04-12 01:03:25 +0200

    timer v1.1 (just one atom, only one timeout is set)

--- src/seven_guis_cljs/task4timer.cljs
+++ src/seven_guis_cljs/task4timer.cljs
@@ -4,38 +4,52 @@
 (defn event->target-value [e]
   (-> e .-target .-value))
 
-(def elapsed-time (r/atom 0.0))
-(def duration (r/atom 15.0))
+(def default-state {:elapsed-time 0.0
+                    :duration 15.0})
+(def state (r/atom default-state))
+
+(defn get-elapsed-time-cursor []
+  (r/cursor state [:elapsed-time]))
 
 (defn elapsed-time-gauge []
   [:div {:style {:font-size "1.5em" :margin "4px"}}
    "Elapsed Time: "
-   [:meter {:value @elapsed-time :min 0 :max @duration :step 0.1}]])
+   [:meter {:value (:elapsed-time @state) :min 0 :max (:duration @state) :step 0.1}]])
 
 (defn inc-zero-point-one [n]
   (/ (+ (* 10 n) 1) 10))
 
-(defn elapsed-time-numerical [tick]
-  (js/setTimeout #(when
-                   tick
-                    (swap! elapsed-time inc-zero-point-one)) 100)
-  [:div {:style {:font-size "1.5em" :margin "4px" :border "2px solid" :padding "4px" :width "6ch"}}
-   (str @elapsed-time "s")])
+(defn elapsed-time-numerical []
+  (let [elapsed-time (get-elapsed-time-cursor)
+        timer (r/atom nil)]
+    (fn []
+      (when (not @timer)
+        (reset! timer (js/setTimeout
+                       #(do (reset! timer nil)
+                            (when (< (:elapsed-time @state) (:duration @state))
+                              (swap! state assoc :elapsed-time (inc-zero-point-one (:elapsed-time @state)))))
+                       100)))
+      [:div {:style {:font-size "1.5em" :margin "4px" :border "2px solid" :padding "4px" :width "6ch"}}
+       (str @elapsed-time "s")])))
 
 (defn duration-slider []
-  [:div {:style {:font-size "1.5em" :margin "4px"}}
-   "Duration: "
-   [:input {:style {:min-width "10%"}
-            :type "range" :value @duration :min 0 :max 15 :step 0.1
-            :on-change #(reset! duration (event->target-value %))}]])
+  (let [duration (r/cursor state [:duration])]
+    (fn []
+      [:div {:style {:font-size "1.5em" :margin "4px"}}
+       "Duration: "
+       [:input {:style {:min-width "10%"}
+                :type "range" :value @duration :min 0 :max (:duration default-state) :step 0.1
+                :on-change #(reset! duration (event->target-value %))}]])))
 
 (defn reset-button []
-  [:button {:style {:font-size "1.5em" :margin "4px" :max-width "50vh"}
-            :on-click #(reset! elapsed-time 0)} "Reset"])
+  (let [elapsed-time (get-elapsed-time-cursor)]
+    (fn []
+      [:button {:style {:font-size "1.5em" :margin "4px" :max-width "50vh"}
+                :on-click #(reset! elapsed-time 0)} "Reset"])))
 
 (defn timer-gui []
   [:div {:class "flex-column-start"}
    [elapsed-time-gauge]
-   [elapsed-time-numerical (< @elapsed-time @duration)]
+   [elapsed-time-numerical]
    [duration-slider]
    [reset-button]])
